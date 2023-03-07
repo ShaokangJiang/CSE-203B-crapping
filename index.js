0561839f927b6be9d6ae2dc8946c3a8b1945bd91
ShaokangJiang <shj002@ucsd.edu>
2023-03-07 00:11:27 -0800

    done

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const HTMLParser = require('node-html-parser');
 const core = require('@actions/core');
 const UIDGenerator = require('uid-generator');
 const uidgen = new UIDGenerator();
-
+const fs = require('fs');
 const dotenv = require("dotenv")
 
 dotenv.config()
- 
+
 const { } = process.env;
 
 
@@ -23,11 +23,10 @@ function replaceAll(originalString, find, replace) {
     return originalString.replace(new RegExp(find, 'g'), replace);
 }
 
-async function mainFunction1() {
+async function mainFunction1(name) {
     // use try catch without timeout at here
     // const browser = await puppeteer.launch({ headless: true , args: [`--no-sandbox`, `--disable-setuid-sandbox`]});\
-    let message = "";
-    let screenshot = true;
+    let screenshot = false;
     let hrstart = process.hrtime();
     browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
@@ -36,7 +35,7 @@ async function mainFunction1() {
     await page.setRequestInterception(true);
 
     page.on('request', (req) => {
-        if (req.resourceType() === 'image') {
+        if (req.resourceType() === 'image' || req.resourceType() === 'media') {
             req.abort();
         }
         else {
@@ -50,18 +49,51 @@ async function mainFunction1() {
 
     hrstart = process.hrtime();
     try {// wait for 60 seconds
-        await page.goto('http://www.google.com', { waitUntil: 'networkidle2', timeout: 60000 }); // wait until page load
+        await page.goto('https://' + name + '.userbenchmark.com/', { waitUntil: 'networkidle2', timeout: 60000 }); // wait until page load
     } catch (e) {
         core.error("Wait too long for login page, terminating the program")
+        await browser.close();
+        core.setFailed(`Action failed with error ${e}`);
+        process.exit(1);
     }
     // await page.waitForTimeout(2000000);
     hrend = process.hrtime(hrstart);
     core.info("Finish loading the first page, first page loaded in " + hrend[0] + "s")
-    let msg = await page.evaluate(async (USERNAME) => {
-        document.getElementById("username").value = USERNAME;
-        // console.log(USERNAME);
-        return;
-    }, USERNAME);
+    core.info("Start to get " + name + " info");
+    let message = await page.evaluate(async () => {
+        function handleNumber(num) {
+            if (num.indexOf("k") != -1) {
+                return (parseFloat(num) * 1000).toFixed(0);
+            } else if (num.indexOf("M") != -1) {
+                return (parseFloat(num) * 1000 * 1000).toFixed(0);
+            } else {
+                return (parseFloat(num)).toFixed(0);
+            }
+        }
+        let toRe = "";
+        document.querySelector("[data-mhth='MC_PRICE']").click()
+        let counter = 0;
+        while (true) {
+            counter++;
+            if (counter >= 60) return "Error";
+            for (let i = 0; i < 7; i++) {
+                await new Promise(resolve => setTimeout(resolve, 1000 + Math.random() * 1500));
+                window.scrollTo(window.scrollX, window.scrollY + 500 + Math.random() * 300);
+            }
+            for (let i of document.getElementsByClassName("hovertarget")) {
+                let lists = i.getElementsByTagName("td");
+                let tempStr = lists[1].innerText.split("\n");
+                //name,price,sample,valuable,bench,bench_low,bench_high
+                if (tempStr[1].indexOf("$") == -1)
+                    return toRe;
+                toRe += tempStr[1].substring(0, tempStr[1].indexOf("$")).trim() + "," + tempStr[1].substring(tempStr[1].indexOf("$") + 1, tempStr[1].length).trim().replace(",", "") + "," + handleNumber(tempStr[2].replace("Samples", "").trim()) + "," + lists[3].innerText.split("\n")[0].trim();
+                tempStr = lists[4].innerText.split("\n");
+                toRe += "," + tempStr[0] + "," + tempStr[1].split("-")[0].trim() + "," + tempStr[1].split("-")[1].trim() + "\n";
+            }
+            document.getElementsByClassName("pagination pagination-lg")[0].lastElementChild.firstChild.click()
+        }
+    });
+    if (message.localeCompare("Error") == 0) throw new Error("Error happened");
 
     await browser.close();
     return message;
@@ -70,25 +102,38 @@ async function mainFunction1() {
 async function main() {
     try {
         let hrstart = process.hrtime();
-        let mainMessage;
-        let count = 1;
-        try {
-            mainMessage = await mainFunction1();
-        } catch (e) {
-            core.error("Error happened: " + e + ", try again");
-            count = count + 1;
-            await browser.close();
+        let requests = ["cpu", "gpu", "ssd", "hdd", "ram"];
+        for (let i of requests) {
+            let mainMessage;
+            let count = 1;
             try {
-                mainMessage = await mainFunction1();
+                mainMessage = await mainFunction1(i);
             } catch (e) {
                 core.error("Error happened: " + e + ", try again");
                 count = count + 1;
                 await browser.close();
-                mainMessage = await mainFunction1();
+                try {
+                    mainMessage = await mainFunction1(i);
+                } catch (e) {
+                    core.error("Error happened: " + e + ", try again");
+                    count = count + 1;
+                    await browser.close();
+                    mainMessage = await mainFunction1(i);
+                }
             }
+            let hrend = process.hrtime(hrstart);
+            core.info("Done " + i + " in " + hrend)
+            // console.log(mainMessage);
+            if (!fs.existsSync('./data')) {
+                await fs.mkdirSync('./data');
+            }
+            await fs.writeFile("./data/" + i + ".csv", "name,price,sample,valuable,bench,bench_low,bench_high\n" + mainMessage, function (err) {
+                if (err) {
+                    return core.info(err);
+                }
+                core.info(i + ".csv was saved!");
+            });
         }
-        let hrend = process.hrtime(hrstart);
-        core.info("Done in" + hrend)
     } catch (e) {
         await browser.close();
         core.setFailed(`Action failed with error ${e}`);
